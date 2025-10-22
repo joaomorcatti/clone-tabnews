9215cf45ae0930ce6fff41276875fe4cb8db8177
João Vitor <joamorcatti@gmail.com>
2025-10-22 17:49:55 -0300

    Adicionanco arquivo .nvmrc

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Meu amor, eu amo você. Se você me mama, dá uma risadinha!</h1>
+    return <h1>Meu amor, eu amo você. Se você me ama, dá uma risadinha!</h1>
 }
 
 export default Home;
