079853465f2e8f2153f5e0f0656781408d5e69af
João Vitor <joamorcatti@gmail.com>
2025-10-22 17:57:43 -0300

    Alterando mensagem do desafio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Meu amor, eu amo você. Se você me ama, dá uma risadinha!</h1>
+    return (
+        <h1>
+        Meu amor, eu amo você. Se você me ama, dá uma risadinha! <br />
+        Fazendo o meu primeiro desafio do projeto.<br />
+        Quando descer me avisa para te entregar as vasilhas!
+        </h1>
+    );
 }
 
 export default Home;
