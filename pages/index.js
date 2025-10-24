f60efe07db45f6315ba74c80dedbd26c475b00fd
João Vitor <joamorcatti@gmail.com>
2025-10-24 13:13:07 -0300

    Adicionado e executado os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 function Home() {
-    return (
-        <h1>
-        Meu amor, eu amo você. Se você me ama, dá uma risadinha! <br />
-        Fazendo o meu primeiro desafio do projeto.<br />
-        Quando descer me avisa para te entregar as vasilhas!
-        </h1>
-    );
+  return <h1>Em construção.</h1>;
+}
+
+function teste() {
+  return <h1>Teste 01</h1>;
 }
 
 export default Home;
