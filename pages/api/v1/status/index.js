5832f23b043ca35c60114b8357a33c99be44eff9
João Vitor <joamorcatti@gmail.com>
2025-11-05 17:56:41 -0300

    Atualizado `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,11 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Primeira API" });
+  const updatedAT = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAT,
+  });
 }
 
 export default status;
